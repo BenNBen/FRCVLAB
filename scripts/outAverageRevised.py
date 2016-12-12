adfa370c59416f6ae5254260a11114e8e90a4ed0
BenNBen <barriageb@gmail.com>
2016-12-12 15:42:13 -0500

    out*Revised scripts debugged and tested

--- scripts/outAverageRevised.py
+++ scripts/outAverageRevised.py
@@ -16,24 +16,29 @@ job = input("Enter the job type: ")
 iMax = input("Enter the iMax number: ")
 theta = input("Enter the angle value: ")
 date = input("Enter the date: ")
+
 f = file(job+"_grid"+grid+"_iMax"+iMax+"_angle"+theta+"_"+date+"Average.csv","wr+")
 average = []
 dist = []
 angle = []
+speed = []
+
 i =0
 for name in glob.glob("*_grid"+"*_iMax"+"*_angle"+"*_*.csv"): # reads in files across all test files following this format within a directory
     with open(name, 'rb') as csvfile:
-        filereader = csv.reader(csvfile, delimiter= ' ')
+        filereader = csv.reader(csvfile, delimiter= ',')
         for row in filereader:
             my_list=row
-            j = float(my_list[-1]) #finds last value of a line and stores it in variable j
-            k = float(my_list[-4]) #finds fourth to last value of a line and stores it in variable k
-            l = float(my_list[-3]) #finds third to last value of a line and stores it in variable l
+            j = float(my_list[-1])
+            l = float(my_list[-2])
+            k = float(my_list[-3])
+            m = float(my_list[-4])
             average.append(j) #adds value of j to overall success value
             dist.append(k) #adds value of k to overall distance from goal value
             angle.append(l) #adds value of l to overall angle offset from goal orientation
+            speed.append(m) #adds value of m to overall speed (num of steps)
             i= i+1
-            #print(j)
+            
 #used for debugging purposes
 print "Total Average"
 print sum(average)/i
@@ -41,7 +46,12 @@ print "Dist Average"
 print sum(dist)/i
 print "Angle Average"
 print sum(angle)/i
-f.write ("Dist average, Angle average, S Average\n")
+print "Speed Average"
+print sum(speed)/i
+
+f.write ("Speed Average, Dist average, Angle average, Success Average\n")
+f.write(str(sum(speed)/i)) #writes total speed average to the output file
+f.write(", ")
 f.write(str(sum(dist)/i)) #writes total distance average to the output file
 f.write(", ")
 f.write(str(sum(angle)/i)) #writes total angle offset average to the output file
--- scripts/outValuesRevised.py
+++ scripts/outValuesRevised.py
@@ -11,10 +11,12 @@ import sys
 
 print "Please make sure to surround all your input with ' '"
 grid = input("Enter the grid number: ")
-iMAX = input("Enter the iMax number: ")
+iMax = input("Enter the iMax number: ")
 theta = input("Enter the angle of the trial: ")
 date = input("Enter the date of the trial: ")
+print "DO NOT surround the following input with ' '"
 cores = input("Enter the number of cores used: ") #cores=number of HSVD out files
+
 def combine(job):
     if (job=="resTrows"):
         num=65 #standard number of lines in resTrows
@@ -24,24 +26,29 @@ def combine(job):
         num=13 #standard number of lines in resTDDiag
     if(job=="resTallSqAng"):
         num=65 #standard number of lines in resTallSqAng
-    f = file(job+"_grid"+grid+"_iMax"+iMax+"_angle"+theta+"_"date+".csv","wr+")
-    data = [[0 for x in xrange (cores)] for x in xrange(num)] #first value needs to change depending on the numer of cores used/result files returned
-    dist = [[0 for x in xrange (cores)] for x in xrange(num)] #first value needs to change depending on the numer of cores used/result files returned
-    angle = [[0 for x in xrange (cores)] for x in xrange(num)]#first value needs to change depending on the numer of cores used/result files returned
+    f = file(job+"_grid"+grid+"_iMax"+iMax+"_angle"+theta+"_"+date+".csv","wr+")
+    data = [[0 for x in xrange (cores)] for x in xrange(num)]
+    dist = [[0 for x in xrange (cores)] for x in xrange(num)]
+    angle = [[0 for x in xrange (cores)] for x in xrange(num)]
+    speed = [[0 for x in xrange (cores)] for x in xrange(num)]
     i = 0
     for name in glob.glob(job+iMax+'/HSVDout*.csv'): #standard results files follow format of the job type and the number of images used in testing (iMax)
         with open(name, 'rb') as csvfile:        
             filereader= csv.reader(csvfile, delimiter= ',')
             for row in filereader: 
                 my_list = row 
-                j = float(my_list[-1]) #checks last value of a line
-                k = float(my_list[-4]) #checks fourth to last value of a line
-                l = float(my_list[-3]) #checks third to last value of a line
+                j = float(my_list[-1]) 
+                l = float(my_list[-3]) 
+                k = float(my_list[-4]) 
+                m = float(my_list[-5]) 
                 data[filereader.line_num][i]=j #adds j's value to data, which will represent the % of success
                 dist[filereader.line_num][i]=k #adds k's value to dist, which will represent the distance away from the goal location
-                angle[filereader.line_num][i]=l #adds l's value to angle, which will represent the offset of the angle from the goal orientation 
+                angle[filereader.line_num][i]=l #adds l's value to angle, which will represent the offset of the angle from the goal orientation
+                speed[filereader.line_num][i]=m #adds m's value to speed, which will represent the number of steps for each run of HSVD
             i = i+1
     for k in range(1,num):
+        f.write(str(sum(speed[k])/cores)) #writes average speed (num of Steps) across the number of files to the ouput file
+        f.write(", ")
         f.write(str(sum(dist[k])/cores)) #writes average success across the number of files to the output file
         f.write(", ")
         f.write(str(sum(angle[k])/cores))#writes average angle offset across the number of files to the output file
@@ -49,8 +56,9 @@ def combine(job):
         f.write(str(sum(data[k])/cores))#writes average distance across the number of files to the output file 
         f.write("\n")
         print sum(data[k])/cores
+        
 #runs through each of the 4 standard scripts used in HSVD testing 
 combine("resTrows")
-combine("resTDiag")
-combine("resTallSqAng")
-combine("resTDDiag")
+#combine("resTDiag")
+#combine("resTallSqAng")
+#combine("resTDDiag")
